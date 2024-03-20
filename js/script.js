604826e0bf73dc23886297b9e12563a6e397e6c5
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-03-20 13:03:37 -0500

    Added the code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============= TYPING ANIMATION =============== */
+let typed = new Typed(".typing", {
+    strings : ["", "Data Scientist", "Machine Learning Engineer"],
+    typeSpeed : 100,
+    BackSpeed : 60,
+    loop : true,
+});
